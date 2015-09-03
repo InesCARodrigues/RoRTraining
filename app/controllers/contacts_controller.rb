ffcd64dd3772500f17fcd46e97116c58b6a405b1
Inês Rodrigues <nocas.rodrigues94@gmail.com>
2015-09-03 21:36:39 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,30 @@ class ContactsController < ApplicationController
     end
     
     def create()
+        @contact = Contact.new(contact_params())
+        
+        if @contact.save    # This is a Commit
+        
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name,email,body).deliver
+        
+            flash[:success] = "Message sent! :D"
+            redirect_to new_contact_path
+        else
+            flash[:danger] = "Oops! Error occured :( Message not sent"
+            redirect_to new_contact_path
+        end
     end
+    
+    
+    private def contact_params()
+        # White list for security reasons
+        params.require(:contact).permit(:name, :email, :comments)
+    end
+
     
     
 end
