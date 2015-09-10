9ea449178c566152afc2dda413b2557e175166ed
Inês Rodrigues <nocas.rodrigues94@gmail.com>
2015-09-10 06:45:03 +0000

    Improved users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
     
     before_action :authenticate_user!
 
+    def index   #Index é usado sempre que queremos listar tudo de um certo controlador
+        @users = User.all
+    end
     
     def show
         @user = User.find(params[:id])
